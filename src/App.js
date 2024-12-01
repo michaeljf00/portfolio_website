db5475f99a29be2340504bdc38cdbcc105753121
Michael Joshua Fababeir <michaeljoshua@Michaels-MacBook-Pro-6.local>
2024-12-01 01:26:18 -0500

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './assets/logo.svg';
+import michaeljoshpf from './assets/michaeljoshpf.jpg';
+import linkedinIcon from './assets/nav-icon.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+function TestButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1)
+    alert("Clicked - going to home page")
+  }
+  return (
+    <button onClick={handleClick}>
+      Home Page
+    </button>
+  );
+}
+
+const ProfileInfo = { name: 'Michael', skills: "Full Stack, Data Science", degrees: "Computer science, Cognitive Science" }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={michaeljoshpf} className="Profile-photo" alt="profile" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +39,10 @@ function App() {
         >
           Learn React
         </a>
+        <div className="social-icon"> 
+          <a hrf="#"><img src={linkedinIcon} alt="" /></a>
+        </div>
+        <TestButton/>
       </header>
     </div>
   );
