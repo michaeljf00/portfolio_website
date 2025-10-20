d323c80bbeed5e5f0da5150f2a9185ce74772be1
michaeljf00 <michaeljfababeir@gmail.com>
2025-10-19 22:56:51 -0400

    Router templates

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            {/* <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/projects" element={<Projects/>} />
+            <Route path="/contact" element={<Contact/>} /> */}
+        </Routes>
+    );
+}
+
+
+export default Routes;
